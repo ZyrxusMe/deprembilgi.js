545e00f79e55c470d95a36ea140839c255f9b29c
Zyrxus <68481939+Zyrxus@users.noreply.github.com>
2022-11-22 15:01:07 +0300

    first commit

---
+++ src/test/app.js
@@ -0,0 +1,15 @@
+const dbjs = require("../app.js") // deprembilgi.js
+
+let limit = 1
+// Deprem Bilgi Al
+setTimeout(async() => {
+    const deprem = await dbjs.al(limit)
+
+console.log(deprem)
+}, 15000);
+
+// Deprem Bilgi Sürüm
+
+const sürüm = dbjs.sürüm()
+
+console.log(sürüm)
